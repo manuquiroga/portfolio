9f2618a62e53e450f341c3f22cc48ac125abdf6f
Manuel Quiroga <quirogamanuel123@gmail.com>
2023-11-18 21:55:40 -0300

    Header component, framer-motion dep

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/header';
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-white dark:bg-gray-900 text-white-50 relative`}>
+        <Header></Header>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
---
+++ lib/data.ts
@@ -0,0 +1,34 @@
+export const links = [
+  {
+    name: "Home",
+    href: "#home",
+  },
+  {
+    name: "About",
+    href: "#about",
+  },
+  {
+    name: "Projects",
+    href: "#projects",
+  },
+  {
+    name: "Skills",
+    href: "#skills",
+  },
+  {
+    name: "Contact",
+    href: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+
+] as const;
+
+export const projectsData = [
+ 
+] as const;
+
+export const skillsData = [
+
+] as const;
