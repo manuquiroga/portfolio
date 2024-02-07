29817eafade7903705ff79bd05209929aadd58b3
Manuel Quiroga <quirogamanuel123@gmail.com>
2024-02-07 19:01:38 -0300

    pfp change

--- components/about.tsx
+++ components/about.tsx
@@ -7,7 +7,8 @@ import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function About() {
-  const imageUrl = "https://i.ibb.co/PgDG6s4/IMG-20230531-161812-460-2.webp";
+  const imageUrl =
+    "https://i.ibb.co/S6SxZr9/20240201-151741-magicstudio-oulrm159e5e.png";
   const { ref } = useSectionInView("About");
 
   return (
@@ -22,24 +23,30 @@ export default function About() {
       <ComponentHeading title="About me"></ComponentHeading>
       <div className="flex justify-center items-center gap-16 flex-col sm:flex-row">
         <div className="w-[250px] h-[250px] rounded-full object-cover">
-          <img src={imageUrl} alt="Profile image" className="w-full h-full object-cover max-w-full rounded-full"></img>
+          <img
+            src={imageUrl}
+            alt="Profile image"
+            className="w-full h-full object-cover max-w-full rounded-full "
+            style={{ objectPosition: "center 40%" }}
+          ></img>
         </div>
 
         <div className="flex flex-col gap-3 w-[23rem] sm:w-[35rem]">
           <p className="text-white text-lg font-medium ">
-            I was born in Argentina on June 10, 2002, and I graduated as a technical programmer from the National
-            Technological University (UTN) in 2023.
+            I was born in Argentina on June 10, 2002, and I
+            graduated as a technical programmer from the
+            National Technological University (UTN) in 2023.
           </p>
           {/* <p className="text-white text-lg font-medium">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing
             elit.
           </p> */}
           <p className="text-white text-lg font-medium">
-            When I&apos;m not coding, I enjoy gaming, playing the guitar, and going to the gym.
+            When I&apos;m not coding, I enjoy gaming,
+            playing the guitar, and going to the gym.
           </p>
         </div>
       </div>
     </motion.section>
   );
 }
-
